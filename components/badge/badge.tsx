49a87c4242034ff041257939dd23169993285fe4
Moses <imunal12345@gmail.com>
2024-10-25 15:06:31 +0200

    migrate to nextjs 15

--- components/badge/badge.tsx
+++ components/badge/badge.tsx
@@ -1,6 +1,6 @@
 export const Badge = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="text-center rounded-md p-1 bg-gray-50 dark:bg-gray-800">
+    <div className="text-center rounded-md p-1 bg-gray-200 dark:bg-gray-800">
       <span className="text-xs font-mono text-gray-500 dark:text-white">
         {children}
       </span>
